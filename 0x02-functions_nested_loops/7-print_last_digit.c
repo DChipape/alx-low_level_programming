5bd625755d132510b8c0d503d53f1ed5d73bc1ed
dchipape <dimakatsosharon94@gmail.com>
2023-04-25 15:32:10 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,15 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_last_digit - print last digit of integer
- * @n: integer argument
- * Return: last digit of number
- */
+* print_last_digit - print last digit of integer
+* @n: integer argument
+* Return: last digit of number
+*/
 
 int print_last_digit(int n)
 {
 	if (n < 0)
-		n *= -1;
+	n *= -1;
 
        	_putchar('0' + (n % 10));
 
