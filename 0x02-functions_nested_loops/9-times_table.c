5799a25ff89cba67d18494f5a483b12a5aa5ac33
dchipape <dimakatsosharon94@gmail.com>
2023-04-25 16:21:30 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 
 /**
- * times_table - print multiplication table
- */
+* times_table - print multiplication table
+*/
 
 void times_table(void)
 {
@@ -12,29 +12,29 @@ void times_table(void)
 
 	for (row = 0; row <= 9; row++)
 	{
-		for (column = 0; column <= 9; column++)
-		{
-			product = (row * column);
+	for (column = 0; column <= 9; column++)
+	{
+	product = (row * column);
 
-			if (column == 0)
-			{
-				_putchar('0' + product);
-			}
-			else if (product <= 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + product);
-			}
-			else if (product > 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar('0' + (product / 10));
-				_putchar('0' + (product % 10));
-			}
-		}
-		_putchar('\n');
+	if (column == 0)
+	{
+	_putchar('0' + product);
+	}
+	else if (product <= 9)
+	{
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+	_putchar('0' + product);
+	}
+	else if (product > 9)
+	{
+	_putchar(',');
+	_putchar(' ');
+	_putchar('0' + (product / 10));
+	_putchar('0' + (product % 10));
+	}
+	}
+	_putchar('\n');
 	}
 }
