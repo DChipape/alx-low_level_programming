f7021967456f39865b1d2b95c3ae1018f1389c33
dchipape <dimakatsosharon94@gmail.com>
2023-04-25 17:22:58 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,7 +19,7 @@ void print_times_table(int n)
 	{
 	_putchar(k + '0');
 	}
-	
+
 	else if (k < 10 && j != 0)
 	{
 	_putchar(',');
@@ -28,7 +28,7 @@ void print_times_table(int n)
 	_putchar(' ');
 	_putchar(k + '0');
 	}
-	
+
 	else if (k >= 10 && k < 100)
 	{
 	_putchar(',');
@@ -37,7 +37,7 @@ void print_times_table(int n)
 	_putchar((k / 10) + '0');
 	_putchar((k % 10) + '0');
 	}
-	
+
 	else if (k >= 100)
 	{
 	_putchar(',');
@@ -49,5 +49,5 @@ void print_times_table(int n)
 	}
 	_putchar('\n');
 	}
-	}
+}
 }
