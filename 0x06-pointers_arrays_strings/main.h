ab421f58c037c9c4c1d432b251fc5669551d332e
dchipape <dimakatsosharon94@gmail.com>
2023-05-04 09:24:15 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,8 +10,7 @@ char *string_toupper(char *);
 char *cap_string(char *);
 int _putchar(char);
 void print_number(int n);
-void print_buffer(char *b, int size);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-char *rot13(char *a);
+char *rot13(char *s);
+char *leet(char *s);
 
 #endif
