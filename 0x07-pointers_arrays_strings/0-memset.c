0e9a79e207669803e57ecae9d2794a4b6de98be1
dchipape <dimakatsosharon94@gmail.com>
2023-05-08 13:48:04 -0700

    0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,14 +4,16 @@
 *@s: Starting address of memory to be filled
 *@b: desired value
 *@n: Bytes number to be changed
-*Return: Changed array with new value for
+*Return: changed array with new value for n bytes
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-int i = 0;
-for (; n > 0; i++)
-s[i] = b;
-n--;
-}
-return (s);
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
 }
