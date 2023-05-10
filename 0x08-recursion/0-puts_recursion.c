9deb1992089c8ac55576192c9503a38689c5349b
dchipape <dimakatsosharon94@gmail.com>
2023-05-10 10:33:09 -0700

    C - Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,21 +1,17 @@
 #include "main.h"
-
 /**
- * _puts_recursion - print a string, followed by new line
- * @s: string to be printed
- *
- * Return: Always 0 (Success)
- */
-
+* _puts_recursion - print a string function, like puts();
+* @s: string to be printed
+* Return: Always 0 (Success)
+*/
 void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar(*s);
-		s++;
-		_puts_recursion(s);
-	}
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
 
-	else
-		_putchar('\n');
+_putchar(*s);
+_puts_recursion(s + 1);
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -20,5 +20,6 @@ int wildcmp(char *s1, char *s2);
 
 int is_palindrome(char *s);
 
+int _putchar(char c);
 
 #endif
